ad2931e5c41035937be16982378758fddb526b78
BluDay <19403893+bluday@users.noreply.github.com>
2025-11-21 14:08:08 +0100

    feat: improve neovim configs

--- config/nvim/lua/bluday/options.lua
+++ config/nvim/lua/bluday/options.lua
@@ -1,15 +1,16 @@
 local indent_width = 4
 
-vim.opt.background = dark
-vim.opt.fileformat = 'unix'
-vim.opt.number = true
-vim.opt.swapfile = false
-vim.opt.termguicolors = true
-vim.opt.wrap = false
+vim.o.background    = dark
+vim.o.fileformat    = 'unix'
+vim.o.number        = true
+vim.o.swapfile      = false
+vim.o.termguicolors = true
+vim.o.visualbell    = true
+vim.o.wrap          = false
 
 vim.opt.fillchars:append { vert = " " }
 
 -- Tabs versus spaces
-vim.opt.expandtab = true
-vim.opt.shiftwidth = indent_width
-vim.opt.tabstop = indent_width
+vim.o.expandtab  = true
+vim.o.shiftwidth = indent_width
+vim.o.tabstop    = indent_width
