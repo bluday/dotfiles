078b6170ef7f86f3a49c6bafe7f45894faf9f8b2
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-01 03:08:14 +0100

    fix(nvim): typo in keymaps file

--- config/nvim/lua/bluday/keymaps.lua
+++ config/nvim/lua/bluday/keymaps.lua
@@ -3,13 +3,14 @@ local map = vim.keymap.set
 local telescope_builtin = require("telescope.builtin")
 
 map("n", "<Space>",   "<Nop>")
-map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+map("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>")
 map("n", "<leader>ff", telescope_builtin.find_files)
-map("n", "<leader>t", "<cmd>term<CR>")
-map("n", "<leader>x", "<cmd>qa!<CR>")
-map("n", "<leader>z", "<cmd>ZenMode<CR")
-map("n", "<Tab>",     "<cmd>bn<CR>")
-map("n", "<S-Tab>",   "<cmd>bp<CR>")
+map("n", "<leader>s", "<Cmd>w<CR>")
+map("n", "<leader>t", "<Cmd>term<CR>")
+map("n", "<leader>x", "<Cmd>qa!<CR>")
+map("n", "<leader>z", "<Cmd>ZenMode<CR>")
+map("n", "<Tab>",     "<Cmd>bn<CR>")
+map("n", "<S-Tab>",   "<Cmd>bp<CR>")
 
 map("t", "<leader>t", "<C-\\><C-n>")
 
