f926fd0eae54de5e26593143bb0e3387e4a18d1c
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-02 22:57:52 +0100

    feat(scripts): notify of screenshot capture

--- scripts/grim_png_screenshot.sh
+++ scripts/grim_png_screenshot.sh
@@ -4,4 +4,14 @@ DATETIME=`date +'%Y-%m-%d_%H:%M:%S'`
 
 FILENAME="$XDG_PICTURES_DIR/Screenshots/screenshot_$DATETIME.png"
 
-pidof grim || grim -c -t png -l 0 "$FILENAME"
+notify() {
+    command -v notify-send > /dev/null 2>&1 && notify-send \
+        "Screenshot captured" \
+        "Your screenshot has been saved successfully."
+}
+
+screenshot() {
+    pidof grim || grim -c -t png -l 0 "$FILENAME"
+}
+
+screenshot && notify
