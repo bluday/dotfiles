1131830efb15fd338f400daef5e644e0801ab504
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-02 18:59:19 +0100

    feat(nvim): configure toggleterm

--- config/nvim/after/plugin/toggleterm.lua
+++ config/nvim/after/plugin/toggleterm.lua
@@ -1,3 +1,8 @@
 require("toggleterm").setup {
-    direction = "horizontal"
+    direction = "horizontal",
+    insert_mappings = true,
+    open_mapping = [[<C-\>]],
+    persist_size = false,
+    size = 20,
+    terminal_mappings = true
 }
