e5b1772f671590d3dbbb7747892a2b3aca266af9
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-02 14:10:57 +0100

    feat(nvim): update key bindings

--- config/nvim/lua/bluday/keymaps.lua
+++ config/nvim/lua/bluday/keymaps.lua
@@ -1,21 +1,19 @@
 local map = vim.keymap.set
 
-local telescope_builtin = require("telescope.builtin")
-
-map("n", "<Space>",   "<Nop>")
-map("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>")
-map("n", "<leader>ff", telescope_builtin.find_files)
-map("n", "<leader>s", "<Cmd>w<CR>")
-map("n", "<leader>t", "<Cmd>ToggleTerm<CR>")
-map("n", "<leader>x", "<Cmd>qa!<CR>")
-map("n", "<leader>z", "<Cmd>ZenMode<CR>")
-map("n", "<Tab>",     "<Cmd>bn<CR>")
-map("n", "<S-Tab>",   "<Cmd>bp<CR>")
-
-map("t", "<leader>t", "<C-\\><C-n>")
+map("n", "<Space>",    "<Nop>")
+map("n", "<leader>e",  "<Cmd>NvimTreeToggle<CR>")
+map("n", "<leader>ff", "<Cmd>Telescope<CR>")
+map("n", "<leader>s",  "<Cmd>w<CR>")
+map("n", "<leader>tt", "<Cmd>ToggleTerm<CR>")
+map("n", "<leader>x",  "<Cmd>qa!<CR>")
+map("n", "<leader>z",  "<Cmd>ZenMode<CR>")
+map("n", "<Tab>",      "<Cmd>bn<CR>")
+map("n", "<S-Tab>",    "<Cmd>bp<CR>")
 
 if vim.g.neovide then
-    map({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-    map({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-    map({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+    local scale_factor_modes = {"n", "v", "t"}
+
+    map(scale_factor_modes, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+    map(scale_factor_modes, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+    map(scale_factor_modes, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
