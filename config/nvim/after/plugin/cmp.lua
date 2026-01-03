9f431074e8741112c5993c3cd49d45c0906817ec
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-03 16:54:29 +0100

    feat(nvim): add `cmp` plugin

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -1,3 +1,32 @@
 local cmp = require("cmp")
 
-cmp.setup()
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+
+            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        end
+    },
+    window = {
+        -- completion    = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"]     = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"]     = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"]     = cmp.mapping.abort(),
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = true })
+    }),
+    sources = cmp.config.sources(
+        {
+            { name = "nvim_lsp" },
+            { name = "vsnip" }
+        },
+        {
+            { name = "buffer" }
+        }
+    )
+})
