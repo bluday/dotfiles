32da71166726720f7b49de2df258bbf5be2cfee0
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-03 21:07:41 +0100

    styleÄ(nvim): sort things in `cmp` after plugin file

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -1,11 +1,12 @@
 local cmp = require("cmp")
 
-cmp.setup({
+cmp.setup {
     snippet = {
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            -- vim.fn["vsnip#anonymous"](args.body)
 
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+            -- For native neovim snippets (Neovim v0.10+)
+            vim.snippet.expand(args.body)
         end
     },
     window = {
@@ -14,19 +15,23 @@ cmp.setup({
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"]     = cmp.mapping.scroll_docs(-4),
+        ["<C-e>"]     = cmp.mapping.abort(),
         ["<C-f>"]     = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"]     = cmp.mapping.abort(),
-        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        --
+        -- Accept currently selected item.
+        --
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        --
         ["<CR>"] = cmp.mapping.confirm({ select = true })
     }),
     sources = cmp.config.sources(
         {
-            { name = "nvim_lsp" },
-            { name = "vsnip" }
+            { name = "nvim_lsp" }
+            -- { name = "vsnip" }
         },
         {
             { name = "buffer" }
         }
     )
-})
+}
