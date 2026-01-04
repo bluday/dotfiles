e86e8f4fbb28ea0a5ab27cf367e5e6a13a9d0c39
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-04 15:29:13 +0100

    feat(nvim): register completion plugins properly

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -8,6 +8,10 @@ cmp.register_source(
 cmp.setup {
     snippet = {
         expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+
+            require('luasnip').lsp_expand(args.body)
+
             vim.snippet.expand(args.body)
         end
     },
@@ -30,6 +34,7 @@ cmp.setup {
     sources = cmp.config.sources(
         {
             { name = "nvim_lsp" },
+            { name = "vsnip" },
             { name = "luasnip" },
             { name = "easy-dotnet" }
         },
