ae7d8e635fcb0e3edac497b54917f2e3ee5c8379
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-09 23:28:35 +0100

    refactor(nvim): simplify config for cmp plugin

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -1,25 +1,22 @@
 local cmp = require("cmp")
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-        end
-    },
-    mapping = cmp.mapping.preset.insert({
+    mapping = {
         ["<C-b>"]     = cmp.mapping.scroll_docs(-4),
         ["<C-e>"]     = cmp.mapping.abort(),
         ["<C-f>"]     = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<CR>"]      = cmp.mapping.confirm({ select = true })
-    }),
-    sources = cmp.config.sources(
-        {
-            { name = "nvim_lsp" },
-            { name = "vsnip" }
-        },
-        {
-            { name = "buffer" }
-        }
-    )
+    },
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+        { name = "path" },
+        { name = "buffer" }
+    }
 }
