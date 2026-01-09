21e7a844d280208cfec7371641db03274cae2e55
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-09 22:42:00 +0100

    refactor(nvim): simplify plugins file

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,85 +1,45 @@
+-- Sorted using ChatGPT.
 return {
-    {
-        "akinsho/bufferline.nvim",
-        dependencies = {
-            "projekt0n/github-nvim-theme"
-        }
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip"
-        }
-    },
-    {
-        "GustavEikaas/easy-dotnet.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim"
-        }
-    },
-    {
-        "tpope/vim-fugitive"
-    },
-    {
-        "projekt0n/github-nvim-theme",
-        name = "github-theme",
-        lazy = false
-    },
-    {
-        "onsails/lspkind.nvim"
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        }
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        build   = "make install_jsregexp",
-        version = "v2.*"
-    },
-    {
-        "mfussenegger/nvim-dap"
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        }
-    },
-    {
-        "mrcjkb/rustaceanvim",
-        lazy    = false,
-        version = "^6"
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        tag = "0.1.8"
-    },
-    {
-        "akinsho/toggleterm.nvim",
-        config  = true,
-        version = "*"
-    },
-    {
-        "xiyaowong/transparent.nvim",
-        lazy = true
-    },
-    {
-        "Mofiqul/vscode.nvim"
-    },
-    {
-        "folke/zen-mode.nvim"
-    }
+    -- Core utilities / shared dependencies
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+
+    -- UI / themes
+    "projekt0n/github-nvim-theme",
+    "Mofiqul/vscode.nvim",
+    "xiyaowong/transparent.nvim",
+
+    -- Completion & snippets
+    "hrsh7th/vim-vsnip",
+    "L3MON4D3/LuaSnip",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-vsnip",
+    "onsails/lspkind.nvim",
+
+    -- LSP / language tooling
+    "neovim/nvim-lspconfig",
+    "mrcjkb/rustaceanvim",
+    "GustavEikaas/easy-dotnet.nvim",
+
+    -- Debugging
+    "mfussenegger/nvim-dap",
+
+    -- Telescope
+    "nvim-telescope/telescope.nvim",
+
+    -- File tree / UI components
+    "nvim-tree/nvim-tree.lua",
+    "nvim-lualine/lualine.nvim",
+    "akinsho/bufferline.nvim",
+
+    -- Git
+    "tpope/vim-fugitive",
+
+    -- Terminal / UX
+    "akinsho/toggleterm.nvim",
+    "folke/zen-mode.nvim"
 }
